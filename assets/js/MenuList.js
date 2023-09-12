f9bbda85e9305549e04e99ea69a23bd72bee2eb0
J-JRT <88273015+J-JRT@users.noreply.github.com>
2023-09-13 02:33:52 +0700

    Update MenuList.js

--- assets/js/MenuList.js
+++ assets/js/MenuList.js
@@ -7,6 +7,21 @@ message_list = [{
 
 item_list = [
     {
+        event: 'HOT',
+        name: 'APIKEY',
+        icon: 'fas fa-terminal',
+        item: [{
+                "url": '/apikey?type=register&name=${đặt tên gì cũng được}',
+                "name": "Create APIKEY"
+            },
+            {
+                "url": '/apikey?type=checker&apikey=${apikey}',
+                "name": "Check APIKEY"
+            }
+        ]
+    },
+    {
+        event: 'HOT',
         name: 'Facebook',
         icon: 'fa-brands fa-facebook',
         item: [{
@@ -25,6 +40,7 @@ item_list = [
         ]
     },
     {
+        event: 'HOT',
         name: 'TikTok',
         icon: 'fa-brands fa-tiktok',
         item: [{
@@ -42,6 +58,7 @@ item_list = [
         ]
     },
     {
+        event: 'HOT',
         name: 'Tạo avatar anime',
         icon: 'fa-solid fa-user-tie',
         item: [{
@@ -49,28 +66,33 @@ item_list = [
                 "name": "Danh sách nhân vật"
             },
             {
-                "url": endpoint + "/taoanhdep/search?type=name&name=nobita",
+                "url": endpoint + "/taoanhdep/search?type=name&name=luffy",
                 "name": "Tìm kiếm nhân vật theo tên"
             },
             {
-                "url": endpoint + "/taoanhdep/search?type=id&id=30",
+                "url": endpoint + "/taoanhdep/search?type=id&id=135",
                 "name": "Tìm kiếm nhân vật theo ID"
             },
             {
-                "url": endpoint + "/taoanhdep/search?type=movie&movie=Conan",
+                "url": endpoint + "/taoanhdep/search?type=movie&movie=One Piece",
                 "name": "Tìm kiếm nhân vật theo tên phim"
             },
             {
-                "url": endpoint + "/taoanhdep/avatarwibu?id=1&chu_nen=Phạm Văn Diện&chu_ky=d-jukie",
+                "url": endpoint + "/taoanhdep/data",
+                "name": "Dữ liệu tạo ảnh anime"
+            },   
+            {
+                "url": endpoint + "/taoanhdep/avatarwibu?id=1&chu_nen=Nguyễn Hải Đăng&chu_ky=JRT",
                 "name": "Tạo avatar"
             }
         ]
     },
     {
+        event: 'HOT',
         name: 'Code',
         icon: 'fa-solid fa-code',
         item: [{
-                "url": 'https://www.phamvandien.xyz/deobfuscate/',
+                "url": 'https://deobfuscate.relative.im/',
                 "name": "Deobfuscate"
             },
             {
@@ -80,43 +102,49 @@ item_list = [
         ]
     },
     {
+        event: 'HOT',
         name: 'Canvas',
         icon: 'fa-solid fa-diagram-lean-canvas',
         item: [{
-                "url": endpoint + "/blink?id=100068432539116,100004253741257&delay=500",
+                "url": endpoint + "/blink?id=100033478361032,100033478361032&delay=500",
                 "name": "Blink"
             },
             {
-                "url": endpoint + "/fbcover/v1?name=Phạm Văn Diện&color=no&address=VietNam&email=exam@gmail.com&subname=D-Jukie&sdt=0325874&uid=100004253741257",
+                "url": endpoint + "/fbcover/v1?name=Nguyễn Hải Đăng&color=cyan&address=VietNam&email=dangz123456789z@gmail.com&subname=JRT&sdt=0396049***&uid=100033478361032",
                 "name": "Facebook Cover v1"
             },
             {
-                "url": endpoint + "/fbcover/v2?name=Phạm Văn Diện&color=no&subname=D-Jukie&id=100",
+                "url": endpoint + "/fbcover/v2?name=Nguyễn Hải Đăng&color=cyan&subname=JRT&id=100033478361032",
                 "name": "Facebook Cover v2"
             },
             {
-                "url": endpoint + "/fbcover/v3?name=Phạm Văn Diện&birthday=18/03&love=Dương Dươngg&location=VietNam&hometown=VietNam&follow=15000000&gender=Nam&uid=100004253741257",
+                "url": endpoint + "/fbcover/v3?name=Nguyễn Hải Đăng&birthday=26/02&love=Nguyễn Hồng Phấn&location=VietNam&hometown=VietNam&follow=37548912&gender=Nam&uid=100033478361032",
                 "name": "Facebook Cover v3"
             },
             {
-                "url": endpoint + "/tiki?text=Phạm Văn Diện",
+                "url": endpoint + "/cccd/?text1=Hải%20Đăng%20-%20JRT&text2=26&text3=02&text4=2003&urlimg=https://i.imgur.com/hvjGX8B.jpg",
+                "name": "Fake CCCD"
+            },   
+            {
+                "url": endpoint + "/tiki?text=Nguyễn Hải Đăng",
                 "name": "Đi cùng Tiki"
             },
             {
-                "url": endpoint + "/thuphap?id=1&sodong=1&dong_1=Pham Van Dien",
+                "url": endpoint + "/thuphap?id=1&sodong=1&dong_1=Nguyễn Hải Đăng",
                 "name": "Chữ Thư Pháp"
             },
             {
-                "url": endpoint + "/giangsinh?text=Văn Diện",
+                "url": endpoint + "/giangsinh?text=Nguyễn Hải Đăng",
                 "name": "Giáng Sinh"
             },
             {
-                "url": endpoint + "/shopmaihuong?text1=Phạm Văn Diện&text2=03322587454",
+                "url": endpoint + "/shopmaihuong?text1=Nguyễn Hải Đăng&text2=0396049***",
                 "name": "Shop Mike Hường"
             }
         ]
     },
     {
+        event: 'HOT',
         name: 'SoundCloud',
         icon: 'fa-brands fa-soundcloud',
         item: [{
@@ -130,10 +158,11 @@ item_list = [
         ]
     },
     {
+        event: 'HOT',
         name: 'Youtube',
         icon: 'fa-brands fa-youtube',
         item: [{
-                "url": endpoint + '/youtube?search=son tung',
+                "url": endpoint + '/youtube?search=Sơn Tùng',
                 "name": "Youtube Search"
             },
             {
@@ -141,17 +170,17 @@ item_list = [
                 "name": "Get Video Id"
             },
             {
-                "url": endpoint + '/youtube?GetVideoDetails=xypzmu5mMPY',
+                "url": endpoint + '/youtube?GetVideoDetails=Jk38OqdAQxc',
                 "name": "Get Video Details"
             },
             {
-                "event": 'Some Error',
                 "url": endpoint + '/youtube?downloadVideo=Jk38OqdAQxc',
                 "name": "Youtube Downloader"
             }
         ]
     },
     {
+        event: 'HOT',
         name: 'Short Url',
         icon: 'fa-solid fa-curling-stone',
         item: [{
@@ -160,44 +189,274 @@ item_list = [
         }]
     },
     {
+        event: 'HOT',
         name: 'Pinterest',
         icon: 'fa-brands fa-pinterest',
         item: [{
-            "url": endpoint + '/pinterest?search=mirai',
+            "url": endpoint + '/pinterest?search=shikimori',
             "name": "Search"
         }]
     },
     {
+        event: 'HOT',
         name: 'Random Images',
         icon: 'fa-solid fa-images',
         item: [{
-                "event": 'NSFW',
-                "url": endpoint + '/images/ausand',
+                "url": endpoint + '/images/anime?apikey=JRTfree_4097397695',
+                "name": "Anime"
+            },
+            {
+                "url": endpoint + '/images/animememe?apikey=JRTfree_4097397695',
+                "name": "Anime Meme"
+            },
+            {
+                "url": endpoint + '/images/animev2?apikey=JRTfree_4097397695',
+                "name": "Anime V2"
+            },
+            {
+                "url": endpoint + '/images/aqua?apikey=JRTfree_4097397695',
+                "name": "Aqua"
+            },
+            {
+                "url": endpoint + '/images/avatar?apikey=JRTfree_4097397695',
+                "name": "Avatar Anime"
+            },
+            {
+                "url": endpoint + '/images/couple?apikey=JRTfree_4097397695',
+                "name": "Couple"
+            },
+            {
+                "url": endpoint + '/images/chitanda?apikey=JRTfree_4097397695',
+                "name": "Chitanda"
+            },
+            {
+                "url": endpoint + '/images/cosplay?apikey=JRTfree_4097397695',
+                "name": "Cosplay"
+            },
+            {
+                "url": endpoint + '/images/gainhat?apikey=JRTfree_4097397695',
+                "name": "Gái nhật"
+            },
+            {
+                "url": endpoint + '/images/gaixinhvn?apikey=JRTfree_4097397695',
+                "name": "Girl Vietnam"
+            },
+            {
+                "url": endpoint + '/images/ghost?apikey=JRTfree_4097397695',
+                "name": "Ghost"
+            },
+            {
+                "url": endpoint + '/images/girl?apikey=JRTfree_4097397695',
+                "name": "Girl"
+            },
+            {
+                "url": endpoint + '/images/hoathinh?apikey=JRTfree_4097397695',
+                "name": "Hoạt hình"
+            },
+            {
+                "url": endpoint + '/images/jack?apikey=JRTfree_4097397695',
+                "name": "JACK 97"
+            },
+            {
+                "url": endpoint + '/images/jimmy?apikey=JRTfree_4097397695',
+                "name": "Jimmy"
+            },
+            {
+                "url": endpoint + '/images/kana?apikey=JRTfree_4097397695',
+                "name": "Kana"
+            },
+            {
+                "url": endpoint + '/images/khanhhuyen?apikey=JRTfree_4097397695',
+                "name": "Khánh Huyền"
+            },
+            {
+                "url": endpoint + '/images/kurumi?apikey=JRTfree_4097397695',
+                "name": "Kurumi"
+            },
+            {
+                "url": endpoint + '/images/lebong?apikey=JRTfree_4097397695',
+                "name": "Lê Bống"
+            },
+            {
+                "url": endpoint + '/images/lienquan?apikey=JRTfree_4097397695',
+                "name": "Liên Quân AOV"
+            },
+            {
+                "url": endpoint + '/images/lnd?apikey=JRTfree_4097397695',
+                "name": "Linh Ngọc Đàm"
+            },
+            {
+                "url": endpoint + '/images/loli?apikey=JRTfree_4097397695',
+                "name": "Loli"
+            },
+            {
+                "url": endpoint + '/images/lolicon?apikey=JRTfree_4097397695',
+                "name": "Lolicon"
+            },
+            {
+                "url": endpoint + '/images/lucy?apikey=JRTfree_4097397695',
+                "name": "Lucy"
+            },
+            {
+                "url": endpoint + '/images/mirai?apikey=JRTfree_4097397695',
+                "name": "Mirai"
+            },
+            {
+                "url": endpoint + '/images/nendienthoai?apikey=JRTfree_4097397695',
+                "name": "Nền điện thoại"
+            },
+            {
+                "url": endpoint + '/images/ngoctrinh?apikey=JRTfree_4097397695',
+                "name": "Ngọc Trinh"
+            },
+            {
+                "url": endpoint + '/images/rem?apikey=JRTfree_4097397695',
+                "name": "Rem"
+            },
+            {
+                "url": endpoint + '/images/sagiri?apikey=JRTfree_4097397695',
+                "name": "Sagiri"
+            },
+            {
+                "url": endpoint + '/images/siesta?apikey=JRTfree_4097397695',
+                "name": "Siesta"
+            },
+            {
+                "url": endpoint + '/images/tokyo?apikey=JRTfree_4097397695',
+                "name": "Tokyo"
+            },
+            {
+                "url": endpoint + '/images/trai?apikey=JRTfree_4097397695',
+                "name": "Trai"
+            },
+            {
+                "url": endpoint + '/images/umaru?apikey=JRTfree_4097397695',
+                "name": "Umaru"
+            },
+            {
+                "url": endpoint + '/images/wallpaper?apikey=JRTfree_4097397695',
+                "name": "Wallpaper"
+            } 
+        ]
+    },
+    {
+        event: 'HOT',
+        name: 'Images NSFW',
+        icon: 'fas fa-images',
+        item: [{
+                "url": endpoint + '/nsfw/ausand?apikey=JRTfree_4097397695',
                 "name": "Ausand"
             },
             {
-                "event": 'NSFW',
-                "url": endpoint + '/images/du',
-                "name": "NSFW 1"
+                "url": endpoint + '/nsfw/butter?apikey=JRTfree_4097397695',
+                "name": "Butter"
             },
             {
-                "event": 'NSFW',
-                "url": endpoint + '/images/girl',
-                "name": "NSFW 2"
+                "url": endpoint + '/nsfw/du?apikey=JRTfree_4097397695',
+                "name": "Ngực"
             },
             {
-                "event": 'NSFW',
-                "url": endpoint + '/images/mong',
-                "name": "NSFW 3"
+                "url": endpoint + '/nsfw/gaisexy?apikey=JRTfree_4097397695',
+                "name": "Gái Sexy"
             },
             {
-                "event": 'NSFW',
-                "url": endpoint + '/images/anime',
-                "name": "Anime"
+                "url": endpoint + '/nsfw/hana?apikey=JRTfree_4097397695',
+                "name": "Hana"
+            },
+            {
+                "url": endpoint + '/nsfw/hentaiz?apikey=JRTfree_4097397695',
+                "name": "Hentaiz"
+            },
+            {
+                "url": endpoint + '/nsfw/ig?apikey=JRTfree_4097397695',
+                "name": "Instagram"
+            },
+            {
+                "url": endpoint + '/nsfw/mong?apikey=JRTfree_4097397695',
+                "name": "Mông"
+            },
+            {
+                "url": endpoint + '/nsfw/naughty?apikey=JRTfree_4097397695',
+                "name": "Naughty"
+            },
+            {
+                "url": endpoint + '/nsfw/nsfw?apikey=JRTfree_4097397695',
+                "name": "NSFW"
+            },
+            {
+                "url": endpoint + '/nsfw/nude?apikey=JRTfree_4097397695',
+                "name": "Nude"
+            },
+            {
+                "url": endpoint + '/nsfw/penissucking?apikey=JRTfree_4097397695',
+                "name": "Penissucking"
+            },
+            {
+                "url": endpoint + '/nsfw/sex?apikey=JRTfree_4097397695',
+                "name": "Sex 18+"
+            },
+            {
+                "url": endpoint + '/nsfw/tw?apikey=JRTfree_4097397695',
+                "name": "Twitter"
+            },
+            {
+                "url": endpoint + '/nsfw/vsbg?apikey=JRTfree_4097397695',
+                "name": "VSBG"
+            },
+            {
+                "url": endpoint + '/nsfw/xnxx?apikey=JRTfree_4097397695',
+                "name": "XNXX"
             }
         ]
     },
     {
+        event: 'HOT',
+        name: 'Composite Video',
+        icon: 'fas fa-photo-video',
+        item: [{
+                "url": endpoint + '/video/anime?apikey=JRTfree_4097397695',
+                "name": "Anime"
+            },
+            {
+                "url": endpoint + '/video/chill?apikey=JRTfree_4097397695',
+                "name": "Chill"
+            },
+            {
+                "url": endpoint + '/video/doraemon?apikey=JRTfree_4097397695',
+                "name": "Doraemon"
+            },
+            {
+                "url": endpoint + '/video/gaixinh?apikey=JRTfree_4097397695',
+                "name": "Gái Xinh"
+            },
+            {
+                "url": endpoint + '/video/genshin?apikey=JRTfree_4097397695',
+                "name": "Genshin"
+            },
+            {
+                "url": endpoint + '/video/goku?apikey=JRTfree_4097397695',
+                "name": "Goku"
+            },
+            {
+                "url": endpoint + '/video/nhacmoingay?apikey=JRTfree_4097397695',
+                "name": "Music"
+            },
+            {
+                "url": endpoint + '/video/nhacytb?apikey=JRTfree_4097397695',
+                "name": "Music Youtube"
+            },
+            {
+                "url": endpoint + '/video/sex?apikey=JRTfree_4097397695',
+                "name": "Sex 18+"
+            },
+            {
+                "url": endpoint + '/video/soundcl?apikey=JRTfree_4097397695',
+                "name": "Soundcloud"
+            }
+        ]
+    },   
+    {
+        event: 'HOT',
         name: 'Simsimi',
         icon: 'fa-solid fa-message-bot',
         item: [{
@@ -211,6 +470,7 @@ item_list = [
         ]
     },
     {
+        event: 'HOT',
         name: 'Games',
         icon: 'fa-solid fa-gamepad',
         item: [{
@@ -252,6 +512,7 @@ item_list = [
         ]
     },
     {
+        event: 'HOT',
         name: 'Tiện ích',
         icon: 'fa-solid fa-lamp-street',
         item: [{
@@ -269,6 +530,7 @@ item_list = [
         ]
     },
     {
+        event: 'HOT',
         name: 'Liên hệ Admin',
         icon: 'fas fa-user-headset',
         item: [{
